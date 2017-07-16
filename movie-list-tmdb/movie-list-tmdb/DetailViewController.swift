509dcecfe7e7f1fcaa7f0471a29fee6ddc74a261
swami701 <swami701search@gmail.com>
2017-07-16 15:46:49 +0800

    INITIAL COMMIT: Intial commit of master-detail vc

---
+++ movie-list-tmdb/movie-list-tmdb/DetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DetailViewController.swift
+//  movie-list-tmdb
+//
+//  Created by Swaminathan Nagarajan on 16/07/17.
+//  Copyright © 2017 Swaminathan Nagarajan. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+  @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+  func configureView() {
+    // Update the user interface for the detail item.
+    if let detail = detailItem {
+        if let label = detailDescriptionLabel {
+            label.text = detail.description
+        }
+    }
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    configureView()
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+  var detailItem: NSDate? {
+    didSet {
+        // Update the view.
+        configureView()
+    }
+  }
+
+
+}
+
